c811f0d840390cb9c260ee340f9b8063f0b6dceb
hanlyjiang <hanlyjiang@outlook.com>
2020-02-29 10:48:19 +0800

    feature：添加复制回到代码编辑器的功能

--- onenote-copy.popclipext/oneonte_copy_back.php
+++ onenote-copy.popclipext/oneonte_copy_back.php
@@ -1,13 +1,12 @@
 <?php
 mb_internal_encoding("UTF-8");
 $input=getenv('POPCLIP_TEXT');
-
+// $input=getenv('POPCLIP_HTML');
 function trim_blank($str){
     //partern的第一位是个空格
-    $partern = '/[ \x{00a0}\x{1680}\x{2000}-\x{200a}\x{202f}\x{205f}\x{3000}\x{2060}]/u';
-    return preg_replace($partern,' ', $str);
+    $partern = '/[ \x{00a0}]/u';
+    return preg_replace($partern,' ', $str);
 }
-
 $result=trim_blank($input);
 echo $result;
 
